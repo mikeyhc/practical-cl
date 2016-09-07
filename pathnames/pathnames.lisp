52b1c6d04868ec53bb55e75d940cdb813fa5b9c9
Michael Blockley <michaelblockley@catalyst.net.nz>
2016-09-08 09:09:21 +1200

    completed pathnames practical

---
+++ pathnames/pathnames.lisp
@@ -0,0 +1,113 @@
+(in-package :cl-user)
+
+(defpackage :com.gigamonkeys.pathnames
+  (:use :common-lisp)
+  (:export
+    :list-directories
+    :file-exists-p
+    :directory-pathname-p
+    :file-pathname-p
+    :pathname-as-directory
+    :pathname-as-file
+    :walk-directory
+    :directory-p
+    :file-p))
+
+(defun component-present-p (value)
+  (and value (not (eql value :unspecified))))
+
+(defun directory-pathname-p (p)
+  (and
+    (not (component-present-p (pathname-name p)))
+    (not (component-present-p (pathname-type p)))))
+
+(defun directory-wildcard (dirname)
+  (make-pathname
+    :name :wild
+    :type #-clisp :wild #+clisp NIL
+    :defaults (pathname-as-directory dirname)))
+
+(defun pathname-as-directory (name)
+  (let ((pathname (pathname name)))
+    (when (wild-pathname-p pathname)
+      (error "Can't reliably convert wild pathnames."))
+    (if (not (directory-pathname-p name))
+      (make-pathname
+        :directory (append (or (pathname-directory pathname) (list :relative)))
+        :name      NIL
+        :type      NIL
+        :defaults  pathname)
+      pathname)))
+
+#+clisp
+(defun clisp-subdirectories-wildcard (wildcard)
+  (make-pathname
+    :directory (append (pathname-directory wildcard) (list :wild))
+    :name NIL
+    :type NIL
+    :defaults wildcard))
+
+(defun list-directory (dirname)
+  (when (wild-pathname-p dirname)
+    (error "Can only list concrete directory names."))
+  (let ((wildcard (directory-wildcard dirname)))
+
+    #+(or sbcl cmu lispworks)
+    (directory wildcard)
+
+    #+openmcl
+    (directory wildcard :directories t)
+
+    #+allegro
+    (directory wildcard :directories-are-files NIL)
+
+    #+clisp
+    (nconc
+      (directory wildcard)
+      (directory (clisp-subdirectories-wildcard wildcard)))
+
+    #-(or sbcl lispworks openmcl allegro clisp)
+    (error "list-directory not implemented")))
+
+(defun file-exists-p (pathname)
+  #+(or sbcl lispworks openmcl)
+  (probe-file pathname)
+
+  #+(or allegro cmu)
+  (or (probe-file (pathname-as-directory pathname))
+      (probe-file pathname))
+
+  #+clisp
+  (or (ignore-errors (probe-file (pathname-as-file pathname)))
+      (ignore-errors
+        (let ((directory-form (pathname-as-directory pathanme)))
+          (when (ext:probe-directory directory-form)
+            directory-form))))
+
+  #-(or sbcl cmu lispworks openmcl allegro clisp)
+  (error "file-exists-p not implemented"))
+
+(defun pathname-as-file (name)
+  (let ((pathname (pathname name)))
+    (when (wild-pathname-p pathname)
+      (error "Can't reliably convert wild pathnames."))
+    (if (directory-pathname-p name)
+      (let* ((directory (pathname-directory pathname))
+             (name-and-type (pathname (first (last directory)))))
+        (make-pathname
+          :directory (butlast directory)
+          :name (pathname-name name-and-type)
+          :type (pathname-type name-and-type)
+          :defaults pathname))
+      pathname)))
+
+(defun walk-directory (dirname fn &key directories (test (constantly t)))
+  (labels
+    ((walk (name)
+       (cond
+         ((directory-pathname-p name)
+          (when (and directories (funcall test name))
+            (funcall fn name))
+          (dolist (x (list-directory name)) (walk x)))
+         ((funcall test name) (funcall fn name)))))
+    (walk (pathname-as-directory dirname))))

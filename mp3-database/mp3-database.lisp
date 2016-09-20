e4851672e57e5711ad1fe41378298cacc5add889
mikeyhc <mikeyhc@atmosia.net>
2016-09-20 23:17:50 +1200

    WIP: work on MP3 database

--- mp3-database/mp3-database.lisp
+++ mp3-database/mp3-database.lisp
@@ -166,3 +166,87 @@
           (insert-row (file->row file) db))
       :test #'mp3-p)
     (format t "~&Loaded ~d files into database." count)))
+
+(defun mklist (thing)
+  (if (listp thing) thing (list thing)))
+
+(defun find-column (column-name schema)
+  (or (find column-name schema :key #'name)
+      (error "No column: `~a in schema: ~a" column-name schema)))
+
+(defun extract-schema (column-names schema)
+  (loop for c in column-names collect (find-column c schema)))
+
+(defun restrict-rows (rows where)
+  (remove-if-not where rows))
+
+(defun extractor (schema)
+  (let ((names (mapcar #'name schema)))
+    #'(lambda (row)
+        (loop for c in names collect c collect (getf row c)))))
+
+(defun project-columns (rows schema)
+  (map 'vector (extractor schema) rows))
+
+(defun row-equality-tester (schema)
+  (let ((names (mapcar #'name schema))
+        (tests (mapcar #'equality-predicate schema)))
+    #'(lambda (a b)
+        (loop for name in names and test in tests
+              always (funcall test (getf a name) (getf b name))))))
+
+(defun distinct-rows (rows schema)
+  (remove-duplicates rows :test (row-equality-tester schema)))
+
+(defun row-comparator (column-names schema)
+  (let ((comparators (mapcar #'comparator
+                             (extract-schema column-names schema))))
+    #'(lambda (a b)
+        (loop
+          for name in column-names
+          for comparator in comparators
+          for a-value = (getf a name)
+          for b-value = (getf b name)
+          when (funcall comparator a-value b-value) return t
+          when (funcall comparator b-value a-value) return nil
+          finally (return nil)))))
+
+(defun sorted-rows (rows schema order-by)
+  (sort (copy-seq rows) (row-comparator order-by schema)))
+
+(defun select (&key (columns t) from where distinct order-by)
+  (let ((rows (rows from))
+        (schema (schema from)))
+    (when where
+      (setf rows (restrict-rows rows where)))
+    (unless (eql columns 't)
+      (setf schema (extract-schema (mklist columns) schema))
+      (setf rows (project-columns rows schema)))
+    (when distinct
+      (setf rows (distinct-rows rows schema)))
+    (when order-by
+      (setf rows (sorted-rows rows schema (mklist order-by))))
+    (make-instance 'table :rows rows :schema schema)))
+
+(defun column-matcher (column value)
+  (let ((name (name column))
+        (predicate (equality-predicate column))
+        (normalized (normalize-for-column value column)))
+    #'(lambda (row) (funcall predicate (getf row name) normalized))))
+
+(defun column-matchers (schema names-and-values)
+  (loop for (name value) on names-and-values by #'cddr
+        when value collect
+        (column-matcher (find-column name schema) value)))
+
+(defun matching (target &rest names-and-values)
+  "Build a where function that matches rows with the given column values."
+  (let ((matchers (column-matchers (schema table) names-and-values)))
+    #'(lambda (row)
+        (every #'(lambda (matcher) (funcall matcher row)) matchers))))
+
+(defun in (column-name table)
+  (let ((test (equality-predicate (find-column column-name (schema table))))
+        (values (mape 'list #'(lambda (r) (getf r column-name)) (row table))))
+    #'(lambda (row)
+        (member (getf row column-name) values :test test))))
